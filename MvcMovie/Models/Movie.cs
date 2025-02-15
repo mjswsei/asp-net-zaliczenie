7f2b13e225b05a52a4586e4a93bf043b3a2a90ec
Maciej Szczepanek <maciej.szczepanek@microsoft.wsei.edu.pl>
2025-02-15 15:21:19 +0100

    rating fix

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -22,6 +22,6 @@ public class Movie
 	[RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
 	public string Genre { get; set; }
 
-	[RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+	[StringLength(30), Required]
 	public string Rating { get; set; }
 }
